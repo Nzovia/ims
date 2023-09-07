5db98435a3539987b055ff5550f220d0d799a56f
Nzovia <nicholas.maundu@roamtech.com>
2023-09-07 13:53:04 +0300

    initializing invetory service

---
+++ inventoy_service/src/main/java/com/devnic/inventoy_service/InventoyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devnic.inventoy_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoyServiceApplication.class, args);
+	}
+
+}
---
+++ inventoy_service/src/test/java/com/devnic/inventoy_service/InventoyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devnic.inventoy_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
